861998f130df228bde0c3a8a86139cbaeed2eea1
Vlad <aprprorm@gmail.com>
2021-09-20 16:00:53 +0300

    1

---
+++ 1/1-6.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+n = 2
+b = 3
+n1 = b
+b1 = n
+print(n, b,':',n1, b1)
---
+++ 1/1-7.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+num = 8
+if num%2 == 0:
+  print('Число', num,'чётное')
+elif num%2 != 0: print('Число', num,'не чётное')
